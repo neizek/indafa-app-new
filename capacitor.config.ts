c6ddf133efceb27bec59a9d79519d3c8c24307dd
Pavels <dnicer3@gmail.com>
2025-11-24 12:24:06 +0200

    Installed Preferences and LocalNotifications

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
 	appId: 'com.teweb.indafa',
 	appName: 'Indafa',
-	webDir: 'build'
+	webDir: 'build',
+	plugins: {
+		LocalNotifications: {
+			smallIcon: 'ic_stat_icon'
+		}
+	}
 };
 
 export default config;
