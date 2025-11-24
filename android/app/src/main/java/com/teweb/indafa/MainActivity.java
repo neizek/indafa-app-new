7ee29dbbf3e7a0539b1d98542fd0f3ff6fd90983
Pavels <dnicer3@gmail.com>
2025-11-24 12:02:48 +0200

    first commit

---
+++ android/app/src/main/java/com/teweb/indafa/MainActivity.java
@@ -0,0 +1,5 @@
+package com.teweb.indafa;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.teweb.indafa',
+  appName: 'Indafa',
+  webDir: 'dist'
+};
+
+export default config;
