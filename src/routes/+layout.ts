e96dc2c0b74a10048d3447ec4af46b7aae7776a2
Pavels <dnicer3@gmail.com>
2025-11-24 22:07:13 +0200

    MOREEEEE

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,13 +1,13 @@
 // import { goto } from '$app/navigation';
-// import { ROUTES } from '$lib/constants/routes';
+import { ROUTES } from '$lib/constants/routes';
 // import { callToLoginPopUp } from '$lib/helpers/auth.js';
 // import {
 // 	initAuthListener,
-// 	initSession
-// isAdmin,
-// isOperator,
-// isReviewer,
-// session
+// 	initSession,
+// 	isAdmin,
+// 	isOperator,
+// 	isReviewer,
+// 	session
 // } from '$lib/stores/auth';
 // import { intendedUrl, previousUrl } from '$lib/stores/navigation';
 import { initTheme } from '$lib/stores/theme';
@@ -15,7 +15,7 @@ import { loadTranslations, supportedLocalesOptions } from '$lib/translations/tra
 import { LocalNotifications } from '@capacitor/local-notifications';
 // import { get } from 'svelte/store';
 // import { resolve } from '$app/paths';
-// import preferences from '$lib/helpers/preferences.js';
+import preferences from '$lib/helpers/preferences.js';
 // import { initCarWashes } from '$lib/stores/carWashes';
 // import { onMount } from 'svelte';
 import { Capacitor } from '@capacitor/core';
@@ -26,8 +26,8 @@ export const ssr = false;
 // let storeReady = false;
 
 // onMount(async () => {
-// const savedLocale = await preferences.get<string>('locale');
-loadTranslations(supportedLocalesOptions[0].value);
+const savedLocale = await preferences.get<string>('locale');
+loadTranslations(savedLocale ?? supportedLocalesOptions[0].value);
 initTheme();
 // await initSession();
 // await initCarWashes();
@@ -35,14 +35,14 @@ initTheme();
 // storeReady = true;
 // });
 
-// const adminRoutes = [ROUTES.ADMIN.DASHBOARD, ROUTES.ADMIN.APPOINTMENTS];
-// const operatorRoutes = [ROUTES.OPERATOR];
-// const protectedRoutes = [
-// 	...adminRoutes,
-// 	...operatorRoutes,
-// 	ROUTES.USER.PROFILE,
-// 	ROUTES.APPOINTMENT
-// ];
+const adminRoutes = [ROUTES.ADMIN.DASHBOARD, ROUTES.ADMIN.APPOINTMENTS];
+const operatorRoutes = [ROUTES.OPERATOR];
+const protectedRoutes = [
+	...adminRoutes,
+	...operatorRoutes,
+	ROUTES.USER.PROFILE,
+	ROUTES.APPOINTMENT
+];
 
 // Only check notification permissions if running natively
 if (Capacitor.isNativePlatform()) {
@@ -59,44 +59,31 @@ if (Capacitor.isNativePlatform()) {
 		});
 }
 
-// export async function load(page) {
-// 	// Wait for store initialization
-// 	if (!storeReady) {
-// 		return {}; // Or show a loading UI instead of running guards
-// 	}
+export async function load(page) {
+	// Route protection (middleware)
+	const needsAuth = protectedRoutes.some((route) => page.url.pathname.startsWith(route as string));
+	const operatorsOnly = operatorRoutes.some((route) =>
+		page.url.pathname.startsWith(route as string)
+	);
+	const adminsOnly = adminRoutes.some((route) => page.url.pathname.startsWith(route as string));
 
-// 	// Notification permission for native platforms
-// 	if (Capacitor.isNativePlatform()) {
-// 		LocalNotifications.checkPermissions().then((status) => {
-// 			if (status.display === 'prompt' || status.display === 'prompt-with-rationale') {
-// 				LocalNotifications.requestPermissions();
-// 			}
-// 		});
-// 	}
+	console.log(needsAuth, operatorsOnly, adminsOnly);
+	// Make sure session is loaded before redirecting
+	// if (needsAuth && !get(session)) {
+	// 	const navigateToLink = get(previousUrl) ?? ROUTES.HOME;
+	// 	intendedUrl.set(page.url);
+	// 	await goto(resolve(navigateToLink as '/'), { replaceState: true });
+	// 	callToLoginPopUp();
+	// 	return {};
+	// }
 
-// 	// Route protection (middleware)
-// 	const needsAuth = protectedRoutes.some((route) => page.url.pathname.startsWith(route as string));
-// 	const operatorsOnly = operatorRoutes.some((route) =>
-// 		page.url.pathname.startsWith(route as string)
-// 	);
-// 	const adminsOnly = adminRoutes.some((route) => page.url.pathname.startsWith(route as string));
+	// if (operatorsOnly && !get(isOperator) && !get(isAdmin) && !get(isReviewer)) {
+	// 	goto(resolve(ROUTES.HOME));
+	// 	return {};
+	// }
 
-// 	// Make sure session is loaded before redirecting
-// 	if (needsAuth && !get(session)) {
-// 		const navigateToLink = get(previousUrl) ?? ROUTES.HOME;
-// 		intendedUrl.set(page.url);
-// 		await goto(resolve(navigateToLink as '/'), { replaceState: true });
-// 		callToLoginPopUp();
-// 		return {};
-// 	}
-
-// 	if (operatorsOnly && !get(isOperator) && !get(isAdmin) && !get(isReviewer)) {
-// 		goto(resolve(ROUTES.HOME));
-// 		return {};
-// 	}
-
-// 	if (adminsOnly && !get(isAdmin) && !get(isReviewer)) {
-// 		goto(resolve(ROUTES.HOME));
-// 		return {};
-// 	}
-// }
+	// if (adminsOnly && !get(isAdmin) && !get(isReviewer)) {
+	// 	goto(resolve(ROUTES.HOME));
+	// 	return {};
+	// }
+}
