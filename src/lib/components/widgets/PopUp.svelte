a04d30f1f18da5ffc6536df8767ddb2f70be78d8
Pavels <dnicer3@gmail.com>
2025-11-26 12:28:38 +0200

    Fix for popup

--- src/lib/components/widgets/PopUp.svelte
+++ src/lib/components/widgets/PopUp.svelte
@@ -6,40 +6,39 @@
 	import { fade, fly } from 'svelte/transition';
 </script>
 
-{#if $popUps && $popUps.length > 0}
-	{#each $popUps as popUp (popUp.id)}
-		<!-- svelte-ignore a11y-click-events-have-key-events -->
-		<!-- svelte-ignore a11y-no-static-element-interactions -->
-		<div
-			class="fixed inset-0 z-50 bg-surface-50-950/50 will-change-auto backface-hidden transform-gpu"
-			transition:fade={{ duration: 300 }}
-			on:click|self={() => closePopUp(popUp.id)}
-		>
-			<div
-				class="absolute right-4 bottom-0 left-4 mx-auto mb-4 max-w-sm space-y-4 card bg-surface-50 p-4 shadow-xl dark:bg-surface-900"
-				transition:fly={{ y: 200, duration: 300, easing: cubicOut }}
-			>
-				<header class="flex items-center justify-between">
-					<div class="flex items-center gap-2">
-						{#if popUp.icon}
-							<svelte:component this={popUp.icon} />
-						{/if}
-						<span class="text-lg font-bold">
-							{$t(popUp.title)}
-						</span>
-					</div>
-					<button class="btn-icon preset-tonal" on:click={() => closePopUp(popUp.id)}>
-						<XIcon />
-					</button>
-				</header>
-				<article class="hide-scroll">
-					{#if typeof popUp.content === 'string'}
-						{popUp.content}
-					{:else}
-						<svelte:component this={popUp.content.component} {...popUp.content.props} />
-					{/if}
-				</article>
+{#each $popUps as popUp (popUp.id)}
+	<!-- {#if popUp} -->
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div
+		class="fixed inset-0 z-50 bg-surface-50-950/50 will-change-auto backface-hidden transform-gpu"
+		transition:fade={{ duration: 300 }}
+		on:click|self={() => closePopUp(popUp.id)}
+	></div>
+	<div
+		class="fixed right-4 z-51 bottom-0 left-4 mx-auto mb-4 max-w-sm space-y-4 card bg-surface-50 p-4 shadow-xl dark:bg-surface-900"
+		transition:fly={{ y: 200, duration: 300, easing: cubicOut }}
+	>
+		<header class="flex items-center justify-between">
+			<div class="flex items-center gap-2">
+				{#if popUp.icon}
+					<svelte:component this={popUp.icon} />
+				{/if}
+				<span class="text-lg font-bold">
+					{$t(popUp.title)}
+				</span>
 			</div>
-		</div>
-	{/each}
-{/if}
+			<button class="btn-icon preset-tonal" on:click={() => closePopUp(popUp.id)}>
+				<XIcon />
+			</button>
+		</header>
+		<article class="hide-scroll">
+			{#if typeof popUp.content === 'string'}
+				{popUp.content}
+			{:else}
+				<svelte:component this={popUp.content.component} {...popUp.content.props} />
+			{/if}
+		</article>
+	</div>
+	<!-- {/if} -->
+{/each}
